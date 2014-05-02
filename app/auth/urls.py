e5a90d5a3eeb7587d8062a48ac3d2dcdd4f69520
Tomas <tomas@tomas-Presario-CQ57.(none)>
2014-05-02 13:42:07 +0200

    comments and minor nit-fixes

--- app/auth/urls.py
+++ app/auth/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('app.auth.views',
     url(r'^register$', 'signup_action', name='register'),
     url(r'^set_email$', 'set_email', name='set_email'),
     url(r'^resend_confirmation_email$', 'resend_confirmation_email', name='resend_confirmation_email'),
-    url(r'^confirmation/(?P<username>.+)/(?P<confirmation>[\w-]+)$', 'validate_user', name='confirmation'),
-    url(r'^delete/(?P<username>.+)/(?P<confirmation>[\w-]+)$', 'delete_user', name='delete'), # delete user with confirmation_hash
+    url(r'^confirmation/(?P<username>[\w-].+)/(?P<confirmation>[\w-]+)$', 'validate_user', name='confirmation'),
+    url(r'^delete/(?P<username>[\w-]+)/(?P<confirmation>[\w-]+)$', 'delete_user', name='delete'), # delete user with confirmation_hash
    	
 )
