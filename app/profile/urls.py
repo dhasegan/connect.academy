9e7538335c0f40a473a747a66809956f5ea0450e
dhasegan <danielhasegan@gmail.com>
2014-06-10 13:15:25 -0400

    Fixed display of professors. added link to profiles in a natural way

--- app/profile/urls.py
+++ app/profile/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('app.profile.views',
-    url(r'^profile/(?P<username>[\w-]+)$', 'profile', name='profile'),
+    url(r'^profile/(?P<username>[\w\-\.]+)$', 'profile', name='profile'),
     url(r'^manage_account$', 'manage_account', name='manage_account'),
     url(r'^password_change_action$', 'password_change_action', name='password_change_action'),
     url(r'^username_change_action$', 'username_change_action', name='username_change_action'),
