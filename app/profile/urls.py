170e9ebee4537371ce80377d09f328bcea745dfa
Tomas <tomas@tomas-Presario-CQ57.(none)>
2014-06-11 16:17:49 +0200

    git ignore and profile urls

---
+++ app/profile/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns('app.profile.views',
+    url(r'^profile/(?P<username>.+)$', 'profile', name='profile'),
+    url(r'^manage_account$', 'manage_account', name='manage_account'),
+    url(r'^password_change_action$', 'password_change_action', name='password_change_action'),
+    url(r'^username_change_action$', 'username_change_action', name='username_change_action'),
+    url(r'^name_change_action$', 'name_change_action', name='name_change_action'),
+)
