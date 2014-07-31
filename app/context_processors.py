c78984d3a3026753516c21105af46ee550378c16
Daniel Hasegan <danielhasegan@gmail.com>
2014-07-31 18:17:29 +0300

    moved context processor to its place

--- app/context_processors.py
+++ app/context_processors.py
@@ -23,3 +23,50 @@ def user_authenticated(request):
                 context['warning'] = render_to_string('objects/notifications/auth/email_not_activated.html', {})
 
     return context
+
+def student_dashboard_context(request, user):
+    context = {
+        'courses': []
+    }
+
+    registrations = StudentCourseRegistration.objects.filter(student=user)
+    for reg in registrations:
+        context['courses'].append({'course': reg.course, 'is_approved': reg.is_approved})
+    return context
+
+def professor_dashboard_context(request, user):
+    context = {
+        'courses': []
+    }
+
+    registrations = ProfessorCourseRegistration.objects.filter(professor=user)
+    for prof_reg in registrations:  # for each professor registration
+        course_dict = {'course': prof_reg.course,
+                       'is_approved': prof_reg.is_approved}
+        if prof_reg.is_approved:
+            course_dict['students'] = {'registered': [], 'pending': []}
+            # for each student registration
+            for student_reg in StudentCourseRegistration.objects.filter(course=prof_reg.course):
+                if student_reg.is_approved:
+                    course_dict['students']['registered'].append(student_reg.student)
+                else:
+                    course_dict['students']['pending'].append(student_reg.student)
+            course_dict['documents'] = prof_reg.course.coursedocument_set.all()
+            course_dict['homework'] = prof_reg.course.coursehomeworkrequest_set.all()
+            course_dict['forum'] = None
+            forums = prof_reg.course.forumcourse_set.all()
+            if forums.count() == 1:
+                course_dict['forum'] = forums[0]
+        context['courses'].append(course_dict)
+
+    return context
+
+def dashboard_context(request):
+    user = jUser.objects.get(id=request.user.id)
+
+    if user.user_type == USER_TYPE_STUDENT:
+        return student_dashboard_context(request, user)
+    elif user.user_type == USER_TYPE_PROFESSOR:
+        return professor_dashboard_context(request, user)
+
+    return {}
--- app/views.py
+++ app/views.py
@@ -9,7 +9,6 @@ from django.views.decorators.http import require_GET, require_POST
 
 from app.models import *
 from app.context_processors import *
-from app.course.context_processors import *
 
 
 def welcome(request):
@@ -35,37 +34,7 @@ def dashboard(request):
         'page': 'dashboard',
         'user_auth': request.user
     }
-    user = request.user
-    context['courses'] = []
-
-    if user.user_type == USER_TYPE_STUDENT:
-        registrations = StudentCourseRegistration.objects.filter(student=user)
-        for reg in registrations:
-            context['courses'].append({'course': reg.course, 'is_approved': reg.is_approved})
-
-    elif user.user_type == USER_TYPE_PROFESSOR:
-        registrations = ProfessorCourseRegistration.objects.filter(professor=user)
-        for prof_reg in registrations:  # for each professor registration
-            course_dict = {'course': prof_reg.course,
-                           'is_approved': prof_reg.is_approved}
-            if prof_reg.is_approved:
-                course_dict['students'] = {'registered': [], 'pending': []}
-                # for each student registration
-                for student_reg in StudentCourseRegistration.objects.filter(course=prof_reg.course):
-                    if student_reg.is_approved:
-                        course_dict['students']['registered'].append(student_reg.student)
-                    else:
-                        course_dict['students']['pending'].append(student_reg.student)
-                course_dict['documents'] = prof_reg.course.coursedocument_set.all()
-                course_dict['homework'] = prof_reg.course.coursehomeworkrequest_set.all()
-                course_dict['forum'] = None
-                forums = prof_reg.course.forumcourse_set.all()
-                if forums.count() == 1:
-                    course_dict['forum'] = forums[0]
-            context['courses'].append(course_dict)
-
-    else:
-        return redirect('/admin')
+    context = dict(context.items() + dashboard_context(request).items())
 
     return render(request, "pages/dashboard.html", context)
 
