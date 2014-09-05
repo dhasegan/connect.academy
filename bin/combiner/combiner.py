ced09ec3c87f470ac8d3d0376fd70e85f94c96fb
Daniel Hasegan <danielhasegan@gmail.com>
2014-09-05 14:56:37 +0300

    less data for demo

--- bin/combiner/combiner.py
+++ bin/combiner/combiner.py
@@ -4,13 +4,13 @@ import json
 fall_courses = json.load( open('courseDetailsFall') )
 spring_courses = json.load( open('courseDetailsSpring') )
 
+all_courses = []
 for course in fall_courses:
-    if 'Catalogue' in course:
-        course['Catalogue'] = "Fall > " + course['Catalogue']
-    else:
-        print "Warning: No Catalogue!"
-
-all_courses = fall_courses + spring_courses
+    if 'Catalogue' in course and 'Electrical Engineering and Computer Science' in course['Catalogue']:
+        all_courses.append(course)
+for course in spring_courses:
+    if 'Catalogue' in course and 'Electrical Engineering and Computer Science' in course['Catalogue']:
+        all_courses.append(course)
 
 all_courses_file = open('courseDetails', 'w')
 all_courses_file.write( json.dumps(all_courses) )
--- bin/populate/populate_professors.py
+++ bin/populate/populate_professors.py
@@ -3,29 +3,35 @@ from app.models import *
 import json
 
 professors = json.load( open('bin/professors/professors_details.json') )
+needed_professors = json.load( open('bin/professors/professors') )
 
 univ = University.objects.filter(name__contains="Jacobs")[0]
 
+used_profs = []
+
+uname_to_prof = {}
+
 for username, details in professors.iteritems():
-    if jUser.objects.filter(username=username).count() > 0:
+    found = None
+    for nprof in needed_professors:
+        if details['lname'] in nprof:
+            found = nprof
+    if not found or found in used_profs:
         continue
-    user = jUser.objects.create_user(username=username, first_name=details['fname'], last_name=details['lname'],
-        password="theotherside01", user_type=USER_TYPE_PROFESSOR, university=univ, is_confirmed=True)
-    if "email" in details:
-        user.email = details['email']
-        user.is_active = True
-    else:
-        user.is_active = False
-    description = ""
-    if "title" in details and details['title']:
-        description = description + "Title: " + details['title'] + "\n"
-    if "deptinfo" in details and details['deptinfo']:
-        description = description + "Department: " + details['deptinfo'] + "\n"
-    if "phone" in details and details['phone']:
-        description = description + "Phone: " + details['phone'] + "\n"
-    if "room" in details and details['room']:
-        description = description + "Room: " + details['room'] + "\n"
-    if description:
-        user.summary = description
+    used_profs.append(found)
+    index = len(used_profs)
+    uname = "professor" + str(index)
+    if jUser.objects.filter(username=uname).count() > 0:
+        continue
+    email = "professor" + str(index) + "@jacobs-university.de"
+    user = jUser.objects.create(username=uname, first_name="Prof.", last_name="P" + str(index),
+        password="1234", user_type=USER_TYPE_PROFESSOR, university=univ, is_confirmed=True, email=email)
+    
     user.save()
+    uname_to_prof["professor" + str(index)] = details
+
+
+uname_file = open("bin/professors/uname_to_prof.json", "w")
+uname_file.write( json.dumps(uname_to_prof) )
+uname_file.close()
 
