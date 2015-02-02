22b4652efa81608392d2ac43b16ed4cfc600658a
Tomas Pllaha <tomaspllaha@gmail.com>
2015-02-02 02:59:31 +0100

    Version Spring 2015: Not sure if stable, but pushing to production anyways. Get ready for 500s :)

--- app/profile/urls.py
+++ app/profile/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('app.profile.views',
-    url(r'^profile/(?P<username>[A-Za-z0-9\._-]{3,25})/$', 'profile', name='profile'),
+    url(r'^profile/(?P<username>[A-Za-z0-9\._-]{2,25})/$', 'profile', name='profile'),
     url(r'^manage_account$', 'manage_account', name='manage_account'),
-    url(r'^profile/(?P<username>[A-Za-z0-9\._-]{3,25})/load_profile_activities$', 'load_profile_activities', name='load_profile_activities'),
-    url(r'^profile/(?P<username>[A-Za-z0-9\._-]{3,25})/load_new_profile_activities$', 'load_new_profile_activities', name='load_new_profile_activities'),
+    url(r'^profile/(?P<username>[A-Za-z0-9\._-]{2,25})/load_profile_activities$', 'load_profile_activities', name='load_profile_activities'),
+    url(r'^profile/(?P<username>[A-Za-z0-9\._-]{2,25})/load_new_profile_activities$', 'load_new_profile_activities', name='load_new_profile_activities'),
     url(r'^edit_profile_summary$', 'edit_summary', name='edit_summary'),
     url(r'^new_profile_picture$', 'new_profile_picture', name='new_profile_picture'),
 
